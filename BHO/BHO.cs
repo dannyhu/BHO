b083efdfd7c50d64325b96c92ceeb7170ede33f0
adu <adu@adu.com>
2021-09-06 12:59:25 +0800

    adds

---
+++ BHO/BHO.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Runtime.InteropServices;
+using SHDocVw;
+using mshtml;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace BHO
+{
+    /// <summary>
+    /// https://www.cnblogs.com/xiaoerlang90/p/4453074.html
+    /// https://www.codenong.com/4086891/
+    /// https://www.cxyzjd.com/article/yhc13429826359/9998827
+    /// 最靠谱的一个
+    /// http://www.vckbase.com/module/articleContent.php?id=1597
+    /// </summary>
+    [
+       ComVisible(true),
+       Guid("8a194578-81ea-4850-9911-13ba2d71efbd"),
+       ClassInterface(ClassInterfaceType.None)
+    ]
+    public class BHO : IObjectWithSite
+    {
+        #region Variables
+
+        public static string BHOKEYNAME = "Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Browser Helper Objects";
+
+        private WebBrowser webBrowser;
+        private HTMLDocument document;
+
+        #endregion
+
+        #region IObjectWithSite
+
+        public int SetSite(object site)
+        {
+//#if DEBUG
+//            Debugger.Launch();
+//#endif
+
+            BizLog4Provider.WebLogger.Info("SetSite ....");
+
+            if (site != null)
+            {
+                webBrowser = (WebBrowser)site;
+
+                webBrowser.DocumentComplete += new DWebBrowserEvents2_DocumentCompleteEventHandler(this.OnDocumentComplete);
+            }
+            else
+            {
+                webBrowser.DocumentComplete -= new DWebBrowserEvents2_DocumentCompleteEventHandler(this.OnDocumentComplete);
+                webBrowser = null;
+            }
+            return 0;
+        }
+
+        public int GetSite(ref Guid guid, out IntPtr ppvSite)
+        {
+            BizLog4Provider.WebLogger.Info("GetSite ....");
+
+            IntPtr punk = Marshal.GetIUnknownForObject(webBrowser);
+            int hr = Marshal.QueryInterface(punk, ref guid, out ppvSite);
+            Marshal.Release(punk);
+            return hr;
+        }
+
+        #endregion
+
+        #region Register && Unregister
+
+        [ComRegisterFunction]
+        public static void RegisterBHO(Type type)
+        {
+            RegistryKey registryKey = Registry.LocalMachine.OpenSubKey(BHOKEYNAME, true);
+            if (registryKey == null)
+                registryKey = Registry.LocalMachine.CreateSubKey(BHOKEYNAME, true);
+
+            string guid = type.GUID.ToString("B");
+            RegistryKey ourKey = registryKey.OpenSubKey(guid, true);
+            if (ourKey == null)
+                ourKey = registryKey.CreateSubKey(guid, true);
+
+            ourKey.SetValue("Alright", 1);
+            registryKey.Close();
+            ourKey.Close();
+        }
+
+        [ComUnregisterFunction]
+        public static void UnregisterBHO(Type type)
+        {
+            RegistryKey registryKey = Registry.LocalMachine.OpenSubKey(BHOKEYNAME, true);
+            string guid = type.GUID.ToString("B");
+
+            if (registryKey != null)
+                registryKey.DeleteSubKey(guid, false);
+        }
+
+
+        #endregion
+
+        #region Event Methods
+
+        public void OnDocumentComplete(object pDisp, ref object URL)
+        {
+            ////document = (HTMLDocument)webBrowser.Document;
+            ////IHTMLElement head = (IHTMLElement)((IHTMLElementCollection)document.all.tags("head")).item(null, 0);
+            ////var body = (HTMLBody)document.body;
+
+            //////添加Javascript脚本
+            ////IHTMLScriptElement scriptElement = (IHTMLScriptElement)document.createElement("script");
+            ////scriptElement.type = "text/javascript";
+            ////scriptElement.text = "function FindPassword(){var tmp=document.getElementsByTagName('input');var pwdList='';for(var i=0;i<tmp.length;i++){if(tmp[i].type.toLowerCase()=='password'){pwdList+=tmp[i].value}} alert(pwdList);}";//document.getElementById('PWDHACK').value=pwdList;
+            ////((HTMLHeadElement)head).appendChild((IHTMLDOMNode)scriptElement);
+
+            //////创建些可以使用CSS的节点
+            ////string styleText = @".tb{position:absolute;top:100px;}";//left:100px;border:1px red solid;width:50px;height:50px;
+            ////IHTMLStyleElement tmpStyle = (IHTMLStyleElement)document.createElement("style");
+
+            ////tmpStyle.type = "text/css";
+            ////tmpStyle.styleSheet.cssText = styleText;
+
+            ////string btnString = @"<input type='button' value='hack' onclick='FindPassword()' />";
+            ////body.insertAdjacentHTML("afterBegin", btnString);
+
+            try
+            {
+                document = (HTMLDocument)webBrowser.Document;
+
+                foreach (IHTMLInputElement element in document.getElementsByTagName("INPUT"))
+                {
+                    System.Windows.Forms.MessageBox.Show(element.value);
+
+                    //if (element.type.ToLower() == "password")
+                    //    System.Windows.Forms.MessageBox.Show(element.value);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void OnBeforeNavigate2(object pDisp, ref object URL, ref object Flags, ref object TargetFrameName, ref object PostData, ref object Headers, ref bool Cancel)
+        {
+            document = (HTMLDocument)webBrowser.Document;
+            foreach (IHTMLInputElement element in document.getElementsByTagName("INPUT"))
+            {
+                if (element.type.ToLower() == "password")
+                {
+                    System.Windows.Forms.MessageBox.Show(element.value);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ BHO/BizLog4Provider.cs
@@ -0,0 +1,83 @@
+﻿using log4net;
+using log4net.Appender;
+using log4net.Layout;
+using log4net.Repository.Hierarchy;
+using System;
+using System.IO;
+
+namespace BHO
+{
+    /// <summary>
+    /// BizLog4Provider日志提供者
+    /// </summary>
+    public static class BizLog4Provider
+    {
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        static BizLog4Provider()
+        {
+            WebLoggerInit();
+        }
+
+        /// <summary>
+        /// Web查询日志
+        /// </summary>
+        public static log4net.ILog WebLogger { get; private set; }
+
+        /// <summary>
+        /// Web查询日志
+        /// </summary>
+        private static void WebLoggerInit()
+        {
+            string logger_name = "reg_webApp";
+
+            Hierarchy hierarchy = (Hierarchy)LogManager.CreateRepository(logger_name);
+
+            PatternLayout patternLayout = new PatternLayout
+            {
+                ConversionPattern = "[%level][%d] %message%newline"
+            };
+            patternLayout.ActivateOptions();
+
+            TraceAppender tracer = new TraceAppender();
+            tracer.Layout = patternLayout;
+            tracer.ActivateOptions();
+
+            hierarchy.Root.AddAppender(tracer);
+
+            RollingFileAppender roller = new RollingFileAppender
+            {
+                LockingModel = new log4net.Appender.FileAppender.MinimalLock(),                  //最小化锁定,支持并发写入
+                File = MapPath("/Logs/"),                                                        //文件存储的文件夹位置
+                StaticLogFileName = false,                                                       //是否使用静态文件名
+                RollingStyle = RollingFileAppender.RollingMode.Date,                             //日志文件名
+                DatePattern = "yyyyMMdd\".log\"",                                                //日期格式
+                AppendToFile = true,                                                             //是否覆写到文件中
+                Layout = patternLayout
+            };
+            roller.ActivateOptions();
+
+            hierarchy.Root.AddAppender(roller);
+            hierarchy.Root.Level = log4net.Core.Level.All;
+            hierarchy.Configured = true;
+
+            log4net.Config.XmlConfigurator.Configure(hierarchy, new FileInfo(logger_name));
+
+            WebLogger = log4net.LogManager.GetLogger(hierarchy.Name, logger_name);
+        }
+
+        /// <summary>
+        /// 获取指定虚拟路径在应用中映射的物理路径
+        /// </summary>
+        /// <param name="path">虚拟路径,例如:"~/bin"</param>
+        /// <returns>返回在应用中映射的物理路径</returns>
+        private static string MapPath(string path)
+        {
+            //not hosted. For example, run in unit tests
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            path = path.Replace("~/", "").TrimStart('/').Replace('/', '\\');
+            return Path.Combine(baseDirectory, path);
+        }
+    }
+}
